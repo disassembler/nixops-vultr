0cc32c8d2b2064bd031c63118e491093e3426d4c
Samuel Leathers <samuel.leathers@iohk.io>
2019-06-13 11:01:22 -0400

    initial commit

---
+++ release.nix
@@ -0,0 +1,52 @@
+{ nixopsSrc ? { outPath = ./.; revCount = 0; shortRev = "abcdef"; rev = "HEAD"; }
+, officialRelease ? false
+, nixpkgs ? <nixpkgs>
+}:
+
+let
+  pkgs = import nixpkgs { };
+  version = "1.6.1" + (if officialRelease then "" else "pre${toString nixopsSrc.revCount}_${nixopsSrc.shortRev}");
+
+in
+
+rec {
+  build = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "x86_64-darwin" ] (system:
+    with import nixpkgs { inherit system; };
+
+    python2Packages.buildPythonPackage rec {
+      name = "nixops-vultr-${version}";
+      namePrefix = "";
+
+      src = ./.;
+
+      prePatch = ''
+        for i in setup.py; do
+          substituteInPlace $i --subst-var-by version ${version}
+        done
+      '';
+
+      buildInputs = [ python2Packages.nose python2Packages.coverage ];
+
+      propagatedBuildInputs = with python2Packages;
+        [
+          vultr
+        ];
+
+      postInstall =
+        ''
+          mkdir -p $out/share/nix/nixops-vultr
+          cp -av nix/* $out/share/nix/nixops-vultr
+        '';
+
+
+      # For "nix-build --run-env".
+      shellHook = ''
+        export PYTHONPATH=$(pwd):$PYTHONPATH
+        export PATH=$(pwd)/scripts:${openssh}/bin:$PATH
+      '';
+
+      doCheck = true;
+
+    });
+
+  }
---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+
+from distutils.core import setup, Command
+
+
+setup(name='nixops-vultr',
+      version='@version@',
+      description='NixOS cloud deployment tool, but for Vultr',
+      url='https://github.com/disassembler/nixops-vultr',
+      author='Samuel Leathers',
+      author_email='disasm@gmail.com',
+      packages=['nixopsvultr.backends'],
+      entry_points={'nixops': ['vultr = nixopsvultr.plugin']},
+      py_modules=['plugin']
+)
